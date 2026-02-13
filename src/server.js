1a71104e016f31e0d6445bce69cd0fad01061183
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-13 20:12:53 +0200

    feat: implement notes routes and environment variables

--- src/server.js
+++ src/server.js
@@ -8,4 +8,19 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+    message: 'Retrieved all notes',
+  });
+});
+
+app.get('/notes/:notesId', (req, res) => {
+  const { noteId } = req.params;
+
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
