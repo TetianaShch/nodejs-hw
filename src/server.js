5de6a7e7692986a18503a1253f879266ab1ddd7e
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-13 20:33:49 +0200

    feat: add middleware (cors, json parser, pino logging)

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+app.use(cors());
+
+app.use(express.json());
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
+
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
@@ -15,10 +26,10 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/notes/:notesId', (req, res) => {
-  const { noteId } = req.params;
+  const { notesId } = req.params;
 
   res.status(200).json({
-    message: `Retrieved note with ID: ${noteId}`,
+    message: `Retrieved note with ID: ${notesId}`,
   });
 });
 
