68af42f9977e4f4ac080f2f14fde27f06066eaf4
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-14 19:53:06 +0200

    fix: rename route param to noteId

--- src/server.js
+++ src/server.js
@@ -24,11 +24,11 @@ app.get('/notes', (req, res) => {
   });
 });
 
-app.get('/notes/:notesId', (req, res) => {
-  const { notesId } = req.params;
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
 
   res.status(200).json({
-    message: `Retrieved note with ID: ${notesId}`,
+    message: `Retrieved note with ID: ${noteId}`,
   });
 });
 
