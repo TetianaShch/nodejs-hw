36ada9730638d8d4e673842a4bca068c9c5c840d
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-21 17:41:46 +0200

    fix: check HttpError instance in error handler middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,15 +1,11 @@
-import createHttpError from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-console.error(err);
+  console.error(err);
 
-  if (createHttpError.isHttpError(err)) {
-    return res.status(err.status).json({
-      message: err.message,
-    });
+  if (err instanceof HttpError) {
+    return res.status(err.statusCode).json({ message: err.message });
   }
-  return res.status(500).json({
-    message: 'Internal Server Error',
-  });
 
+  return res.status(500).json({ message: 'Internal Server Error' });
 };
