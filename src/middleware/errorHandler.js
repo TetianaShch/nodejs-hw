6c06095c381cc863536f2019d6dc448d98f549c0
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-02-21 13:56:02 +0200

    feat: implement notes CRUD with MongoDB

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,9 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  res.status(500).json({
+  const status = err.status || 500;
+
+  res.status(status).json({
     message: err.message || 'Internal Server Error',
   });
 
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
+import notesRouter from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ await connectMongoDB();
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-
+app.use(notesRouter);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({
