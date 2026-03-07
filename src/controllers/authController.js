aaf70c0ac1d5bdb0e9dcc4993a908a56d07c2f80
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-07 22:02:21 +0200

    feat: add refresh user session

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -46,4 +46,30 @@ export const loginUser = async (req, res) => {
   res.status(200).json(user);
 };
 
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+
+  const session = await Session.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  if (new Date() > session.refreshTokenValidUntil) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await Session.deleteOne({ _id: session._id });
+
+  const newSession = await createSession(session.userId);
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({
+    message: 'Session refreshed',
+  });
+};
+
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { loginUser, registerUser } from '../controllers/authController.js';
 import { loginUserSchema, registerUserSchema } from '../validations/authValidation.js';
+import { refreshUserSession } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/register', celebrate(registerUserSchema), registerUser);
 router.post('/login', celebrate(loginUserSchema), loginUser);
+router.post('/refresh', refreshUserSession);
+
 
 export default router;
