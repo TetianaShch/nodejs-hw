b14dea34e6fb13db59c44d8d954dd15503c58ef6
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-14 00:40:39 +0200

    feat: add user avatar upload with cloudinary

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authenticate } from "../middleware/authenticate.js";
 import { updateUserAvatar } from "../controllers/userController.js";
 
-import { upload } from "../middleware/multer";
+import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
