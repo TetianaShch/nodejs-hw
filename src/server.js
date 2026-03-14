b75eb8cc5529255dad6018dbb6238133d3e528de
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-03-14 11:59:06 +0200

    fix: remove route prefixes from router registration

--- src/server.js
+++ src/server.js
@@ -24,9 +24,9 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/auth', authRouter);
-app.use('/users/me', userRouter);
-app.use('/notes', notesRouter);
+app.use(authRouter);
+app.use(userRouter);
+app.use(notesRouter);
 
 app.use(notFoundHandler);
 
